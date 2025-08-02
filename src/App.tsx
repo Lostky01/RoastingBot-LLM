f1618bde431f0a06ae02d25ae9d113803d490871
unknown <sundawarifky@gmail.com>
2025-08-02 12:27:14 +0700

    first

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,11 @@
 // import viteLogo from '/vite.svg'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RoastingMachine from "./pages/RoastingBot";
-import { Toaster } from "sonner";
 import './App.css'
 
 function App() {
   return (
     <Router>
-       <Toaster />
       <Routes>
       <Route path="/" element={<RoastingMachine />} />    
       </Routes>
