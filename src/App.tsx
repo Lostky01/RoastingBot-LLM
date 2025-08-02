6b18d710c1ab8c4a404e651ec0709c47f71533b9
unknown <sundawarifky@gmail.com>
2025-08-02 12:24:20 +0700

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RoastingMachine from "./pages/RoastingBot";
 import { Toaster } from "sonner";
 import './App.css'
